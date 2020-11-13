b3ad29cddf29cb2342606bbd0e244650d9585686
Austin Harvey <harveymaustin@hotmail.com>
2020-11-12 19:43:19 -0600

    Removed load button and resized game over screen

--- Project/Fall2020_CSC403_Project/FrmGameOver.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmGameOver.Designer.cs
@@ -28,25 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(512, 374);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 49);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Load Game";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.loadGame);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(512, 429);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(477, 394);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 49);
+            this.button2.Size = new System.Drawing.Size(216, 82);
             this.button2.TabIndex = 1;
             this.button2.Text = "Exit Game";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,10 +47,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.You_Died;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1165, 551);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.Name = "FrmGameOver";
             this.Text = "FrmGameOver";
@@ -69,8 +58,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
